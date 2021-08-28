f68b7480e6f5e8885edfd8bc9834025671ef5777
hitesh <hitesh22rana@gmail.com>
2021-08-28 22:25:27 +0530

    UI/UX changes

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
 
     pygame.init() 
     FPSCLOCK = pygame.time.Clock()
-    pygame.display.set_caption('Flappy Bird by CodeWithHarry')
+    pygame.display.set_caption('Flappy Bird')
     GAME_SPRITES['numbers'] = ( 
         pygame.image.load('gallery/sprites/0.png').convert_alpha(),
         pygame.image.load('gallery/sprites/1.png').convert_alpha(),
